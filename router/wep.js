304f03b37ade41202fb4bebd97ccd2446c14f655
isaac kamel <isaackamel12345@gmail.com>
2025-01-18 13:33:32 +0200

    Updated wep.js with recent changes

--- router/wep.js
+++ router/wep.js
@@ -42,7 +42,7 @@ router.get("/admin/create", adminAcces, (req, res) => {
 
 router.post("/admin", adminAcces, async (req, res, next) => {
   try {
-    const response = await fetch("http://localhost:5000/cms/admin", {
+    const response = await fetch("https://news-backend-hbbs.onrender.com/cms/admin", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -74,7 +74,7 @@ router.post("/admin", adminAcces, async (req, res, next) => {
 
 router.get("/admin", adminAcces, async (req, res) => {
   try {
-    const response = await fetch("http://localhost:5000/cms/admin");
+    const response = await fetch("https://news-backend-hbbs.onrender.com/cms/admin");
 
     const result = await response.json(); // This is likely an object
     const data = Array.isArray(result.data) ? result.data : []; // Fallback to empty array if data is not an array
@@ -94,7 +94,7 @@ router.get("/:guard/login", guest, async (req, res) => {
   req.session.guard = req.params.guard;
   try {
     const response = await fetch(
-      `http://localhost:5000/cms/${req.params.guard}/login`
+      `https://news-backend-hbbs.onrender.com/cms/${req.params.guard}/login`
     );
     const data = await response.json();
     res.render("layouts/auth/login", { data }); // Pass the data to the EJS template
@@ -106,7 +106,7 @@ router.get("/:guard/login", guest, async (req, res) => {
 router.get("/:guard/SignUP", guest, async (req, res) => {
   req.session.guard = req.params.guard;
   console.log(req.session.guard);
-  const response = await fetch(`http://localhost:5000/cms/user/SignUP`);
+  const response = await fetch(`https://news-backend-hbbs.onrender.com/cms/user/SignUP`);
   const data = await response.json();
   console.log(data);
   res.render("layouts/auth/SignUp", { data }); // Pass the data to the EJS template
@@ -115,7 +115,7 @@ router.get("/:guard/SignUP", guest, async (req, res) => {
 router.post("/:guard/login", guest, async (req, res) => {
   req.session.guard = req.params.guard;
   const response = await fetch(
-    `http://localhost:5000/cms/${req.params.guard}/login`,
+    `https://news-backend-hbbs.onrender.com/cms/${req.params.guard}/login`,
     {
       method: "POST",
       headers: {
@@ -140,7 +140,7 @@ router.post("/:guard/login", guest, async (req, res) => {
 
 router.get("/news", authenticate, async (req, res) => {
   try {
-    const response = await fetch(`http://localhost:5000/cms/news`);
+    const response = await fetch(`https://news-backend-hbbs.onrender.com/cms/news`);
     const data = await response.json();
     res.render("layouts/index", {
       localNews: data.localNews,
@@ -160,7 +160,7 @@ router.get("/news/create", adminAcces, (req, res) => {
 router.get("/news/:id", authenticate, async (req, res) => {
   try {
     const response = await fetch(
-      `http://localhost:5000/cms/news/${req.params.id}`
+      `https://news-backend-hbbs.onrender.com/cms/news/${req.params.id}`
     );
     const result = await response.json();
     const data = result.data;
@@ -174,7 +174,7 @@ router.get("/news/:id", authenticate, async (req, res) => {
 
 router.post("/news", adminAcces, async (req, res, next) => {
   try {
-    const response = await fetch("http://localhost:5000/cms/news", {
+    const response = await fetch("https://news-backend-hbbs.onrender.com/cms/news", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -211,7 +211,7 @@ router.get("/user/create", adminAcces, (req, res) => {
 
 router.post("/user", adminAcces, async (req, res, next) => {
   try {
-    const response = await fetch("http://localhost:5000/cms/user", {
+    const response = await fetch("https://news-backend-hbbs.onrender.com/cms/user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -257,7 +257,7 @@ router.post("/user", adminAcces, async (req, res, next) => {
 
 router.get("/user", adminAcces, async (req, res) => {
   try {
-    const response = await fetch("http://localhost:5000/cms/user");
+    const response = await fetch("https://news-backend-hbbs.onrender.com/cms/user");
 
     const result = await response.json(); // This is likely an object
     const data = Array.isArray(result.data) ? result.data : []; // Fallback to empty array if data is not an array
@@ -277,7 +277,7 @@ router.get("/user", adminAcces, async (req, res) => {
 router.post("/user/:id", adminAcces, async (req, res) => {
   try {
     const response = await fetch(
-      `http://localhost:5000/cms/user/${req.params.id}`,
+      `https://news-backend-hbbs.onrender.com/cms/user/${req.params.id}`,
       {
         method: "POST",
         headers: {
@@ -307,7 +307,7 @@ router.get("/logout", authenticate, (req, res) => {
 router.get("/category/:id", authenticate, async (req, res) => {
   try {
     const response = await fetch(
-      `http://localhost:5000/cms/category/${req.params.id}`
+      `https://news-backend-hbbs.onrender.com/cms/category/${req.params.id}`
     );
 
     const result = await response.json();
